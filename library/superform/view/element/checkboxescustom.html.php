746211e03473b4b90598550894d4a2720589610c
muzudho <muzudho1@gmail.com>
2016-03-01 12:45:47 +0900

    スキルのデータ、ＨＴＭＬの作り直し

--- library/superform/view/element/checkboxescustom.html.php
+++ library/superform/view/element/checkboxescustom.html.php
@@ -1,11 +1,26 @@
-<?php foreach ($this['options'] as $key => $checkbox): ?>
+<?php
+foreach ($this['options'] as $key => $checkbox){
+    if(empty($checkbox->category)){
+        if($key != 0)
+        {
+            // 最初の要素以外は、＜ul＞タグを一旦閉じます。
+            echo '</ul>';
+        }
 
-<?php if(empty($checkbox['category'])) : ?>
-<?php if($key != 0) : ?></ul><?php endif;?>
-<p><?php echo $checkbox->label;?></p>
-<ul<? if (PC_VIEW): ?> class="heightLineParent"<? endif; ?>>
-<?php else : ?>
-    <li><?php echo $checkbox->getInnerHTML() ?></li>
-<?php endif; ?>
-<?php endforeach ?>
-</ul>
+        // 大見出し
+        echo '<p>' . $checkbox->label . '</p>';
+        // 開始ulタグ
+        echo '<ul';
+        if(PC_VIEW){
+            echo ' class="heightLineParent"';
+        }
+        echo '>';
+
+    } else {
+        // 小見出し
+        echo '<li>' . $checkbox->getInnerHTML() . '</li>';
+    }
+}
+
+// 終了ulタグ
+echo '</ul>';
